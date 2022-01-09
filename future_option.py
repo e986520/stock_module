f409fa135763cd880ad87ddab9b7bc6f840e8cd1
e986520 <79308131+e986520@users.noreply.github.com>
2022-01-09 17:18:54 +0800

    Update future_option.py

--- future_option.py
+++ future_option.py
@@ -61,21 +61,22 @@ def to_excel():
     df = df.sort_index()
     df.insert(0, "結算日", 0)
     df.insert(5, "外資期貨留倉與結算差", df["外資期貨留倉"])
-    df.insert(8, "自營期貨留倉與結算差", df["自營期貨留倉"])
+    df.insert(10, "外資SC口數", df["外資BC口數"] - df["外資CALL口數"])
+    df.insert(11, "外資SC金額", df["外資BC金額"] - df["外資CALL金額"])
     df.insert(12, "外資BC金額與結算差", df["外資BC金額"])
-    df.insert(13, "外資SC金額", df["外資BC金額"] - df["外資CALL金額"])
-    df.insert(14, "外資SC金額與結算差", df["外資SC金額"])
-    df.insert(18, "外資BP金額與結算差", df["外資BP金額"])
+    df.insert(13, "外資SC金額與結算差", df["外資SC金額"])
+    df.insert(18, "外資SP口數", df["外資BP口數"] - df["外資PUT口數"])
     df.insert(19, "外資SP金額", df["外資BP金額"] - df["外資PUT金額"])
-    df.insert(20, "外資SP金額與結算差", df["外資SP金額"])
-    df.insert(23, "外資SC/SP比", round(df["外資SC金額"] / df["外資SP金額"], 3))
-    df.insert(27, "自營BC金額與結算差", df["自營BC金額"])
-    df.insert(28, "自營SC金額", df["自營BC金額"] - df["自營CALL金額"])
-    df.insert(29, "自營SC金額與結算差", df["自營SC金額"])
-    df.insert(33, "自營BP金額與結算差", df["自營BP金額"])
-    df.insert(34, "自營SP金額", df["自營BP金額"] - df["自營PUT金額"])
-    df.insert(35, "自營SP金額與結算差", df["自營SP金額"])
-    df.insert(38, "自營SC/SP比", round(df["自營SC金額"] / df["自營SP金額"], 3))
+    df.insert(20, "外資BP金額與結算差", df["外資BP金額"])
+    df.insert(21, "外資SP金額與結算差", df["外資SP金額"])
+    df.insert(24, "外資SC/SP比", round(df["外資SC金額"] / df["外資SP金額"], 3))
+    df.insert(28, "自營BC金額與結算差", df["自營BC金額"])
+    df.insert(29, "自營SC金額", df["自營BC金額"] - df["自營CALL金額"])
+    df.insert(30, "自營SC金額與結算差", df["自營SC金額"])
+    df.insert(34, "自營BP金額與結算差", df["自營BP金額"])
+    df.insert(35, "自營SP金額", df["自營BP金額"] - df["自營PUT金額"])
+    df.insert(36, "自營SP金額與結算差", df["自營SP金額"])
+    df.insert(39, "自營SC/SP比", round(df["自營SC金額"] / df["自營SP金額"], 3))
 
     end_in_df = []
     for i in range(len(end) - 1):
@@ -84,7 +85,6 @@ def to_excel():
             df.loc[end[i], "結算日"] = 1
             if end[i] + pd.to_timedelta("1 day") in df.index:
                 df.loc[end[i] : end[i + 1], "外資期貨留倉與結算差"] = df["外資期貨留倉"] - df.loc[end[i], "外資期貨留倉"]
-                df.loc[end[i] : end[i + 1], "自營期貨留倉與結算差"] = df["自營期貨留倉"] - df.loc[end[i], "自營期貨留倉"]
                 df.loc[end[i] : end[i + 1], "外資BC金額與結算差"] = df["外資BC金額"] - df.loc[end[i], "外資BC金額"]
                 df.loc[end[i] : end[i + 1], "外資SC金額與結算差"] = df["外資SC金額"] - df.loc[end[i], "外資SC金額"]
                 df.loc[end[i] : end[i + 1], "外資BP金額與結算差"] = df["外資BP金額"] - df.loc[end[i], "外資BP金額"]
@@ -94,7 +94,7 @@ def to_excel():
                 df.loc[end[i] : end[i + 1], "自營BP金額與結算差"] = df["自營BP金額"] - df.loc[end[i], "自營BP金額"]
                 df.loc[end[i] : end[i + 1], "自營SP金額與結算差"] = df["自營SP金額"] - df.loc[end[i], "自營SP金額"]
 
-    df = df.drop(["外資BC金額", "外資SC金額", "外資BP金額", "外資SP金額", "自營BC金額", "自營SC金額", "自營BP金額", "自營SP金額"], axis=1)
+    df = df.drop(["自營BC金額", "自營SC金額", "自營BP金額", "自營SP金額"], axis=1)
     df = df.fillna(0)
 
     market = yf.download("^TWII", start=df.index[0], end=(df.index[-1] + pd.to_timedelta("1 day")))
@@ -105,7 +105,9 @@ def to_excel():
     df = df[df.index >= end_in_df[0]]
 
     df.tail(2).to_excel("籌碼更新.xlsx")
+
     return
+
 
 def option_price(date):
     market = 0
@@ -120,13 +122,13 @@ def option_price(date):
     except:
         print("fail!maybe it's a holiday")
         return
-    
+
     if "W" in call.loc[0, "到期月份(週別)"]:
         call = call[call.loc[:, "到期月份(週別)"].str.contains("W")]
     call = call.set_index(["到期月份(週別)"])
     call = call.apply(lambda x: pd.to_numeric(x, errors="coerce"))
     call = call[["履約價", "最後成交價", "*未沖銷契約量"]].dropna()
-    call["換算台指"] = call["履約價"] + call["最後成交價"] - call["最後成交價"].shift(-2)
+    call["換算台指"] = call["履約價"] + call["最後成交價"] - call["最後成交價"].shift(-1)
     call = call.nlargest(4, "*未沖銷契約量")
     call = call.sort_values("履約價")
 
@@ -135,11 +137,13 @@ def option_price(date):
     put = put.set_index(["到期月份(週別)"])
     put = put.apply(lambda x: pd.to_numeric(x, errors="coerce"))
     put = put[["履約價", "最後成交價", "*未沖銷契約量"]].dropna()
-    put["換算台指"] = put["履約價"] - put["最後成交價"] + put["最後成交價"].shift(2)
+    put["換算台指"] = put["履約價"] - put["最後成交價"] + put["最後成交價"].shift(1)
     put = put.nlargest(4, "*未沖銷契約量")
     put = put.sort_values("履約價")
 
     df = pd.concat([put, call])
     df = df.drop("最後成交價", axis=1)
     df.to_excel("大盤支撐壓力更新.xlsx")
+
     return
+
